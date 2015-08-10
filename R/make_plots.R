1859c7923c26c1b969d37e99f775da11e786d214
biorelated <biorelated@gmail.com>
2015-08-10 09:44:50 +0300

    revert to using a line plot to show deduplication,rename the legend from the defaults

--- R/make_plots.R
+++ R/make_plots.R
@@ -3,7 +3,7 @@ require(reshape2)
 source("R/get_data.R")
 
 plot.per.base.sequence.content <- function(data_frame){
- plot <- ggplot(melt(data_frame,id.vars="base"), aes(x = base, y = value, fill =variable )) +
+  plot <- ggplot(melt(data_frame,id.vars="base"), aes(x = base, y = value, fill =variable )) +
     geom_bar(stat = "identity",width = .7) +
     scale_fill_manual(name="",values=c("red","blue","purple","grey30")) +
     theme(axis.text.x  = element_text(size=10),
@@ -11,12 +11,12 @@ plot.per.base.sequence.content <- function(data_frame){
     ylab("proportion") +
     xlab("")
 
- return(plot)
+  return(plot)
 }
 
 plot.per.base.N.count <- function(data_frame){
   plot <- ggplot(data_frame,aes(base,N_count)) +
-    geom_bar(stat = "identity",width=.7) +
+    geom_bar(stat = "identity",width=.7,fill="red") +
     xlab("position in read(bp)") +
     ylab("% ambigous bases")
 
@@ -24,15 +24,17 @@ plot.per.base.N.count <- function(data_frame){
 }
 
 plot.seq.duplication.levels <- function(data_frame){
-  plot <- ggplot(melt(data_frame,id.vars=c("duplication_level")),aes(duplication_level,value,fill=as.factor(variable))) +
-    geom_bar(position="dodge",stat="identity",width=.7) +
-    scale_fill_manual(name="",labels=c("% Deduplicated","% Total"),values = c("red","blue")) +
+  plot <- ggplot(melt(data_frame,id.vars=c("duplication_level")),aes(duplication_level,value,colour=as.factor(variable),ymax=max(value)*1.05)) +
+    #geom_bar(position="dodge",stat="identity",width=.7) +
+    geom_line(position=position_dodge(.1),aes(group=variable)) +
+    #scale_y_discrete(breaks=seq(0, 100, 10)) +
+    scale_colour_manual(name="",labels=c("Distinct","Total"),values = c("red","blue")) +
     ylim(0,100) +
     theme(axis.text.x  = element_text(size=10)) +
     xlab("sequence deduplication level") +
     ylab("")
 
-   return(plot)
+  return(plot)
 }
 
 plot.per.sequence.quality.scores <- function(data_frame){
@@ -54,44 +56,46 @@ plot.per.base.sequence.quality <- function(data_frame){
 }
 
 
-#examples
 json_data1 <- read.fastqc("~/RSV_analysis/test/fastqc/ERR303259_1_fastqc/ERR303259_1_fastqc.json")
 json_data2 <- read.fastqc("~/RSV_analysis/test/fastqc/ERR303259_2_fastqc/ERR303259_2_fastqc.json")
 
 json_data1 <- read.fastqc("~/RSV_analysis/test/fastqc/ERR438932_1_fastqc/ERR438932_1_fastqc.json")
 json_data2 <- read.fastqc("~/RSV_analysis/test/fastqc/ERR438932_2_fastqc/ERR438932_2_fastqc.json")
 
-basic.info1 <- basic_stats(json_data1)
-basic.info2 <- basic_stats(json_data2)
-file1_name <- tools::file_path_sans_ext(basic.info1$file_name)
-file2_name <- tools::file_path_sans_ext(basic.info2$file_name)
+
+  basic.info1 <- basic_stats(json_data1)
+  basic.info2 <- basic_stats(json_data2)
+  file1_name <- tools::file_path_sans_ext(basic.info1$file_name)
+  file2_name <- tools::file_path_sans_ext(basic.info2$file_name)
 
 
-plot_grid(
-  plot.per.base.sequence.quality(per_base_sequence_quality(json_data1)) + ggtitle(file1_name),
-  plot.per.base.sequence.quality(per_base_sequence_quality(json_data2)) + ggtitle(file2_name)
-)
+  plot_grid(
+    plot.per.base.sequence.quality(per_base_sequence_quality(json_data1)) + ggtitle(file1_name),
+    plot.per.base.sequence.quality(per_base_sequence_quality(json_data2)) + ggtitle(file2_name)
+  )
 
-plot_grid(
-  plot.per.sequence.quality.scores(per_sequence_quality_scores(json_data1)) + ggtitle(file1_name),
-  plot.per.sequence.quality.scores(per_sequence_quality_scores(json_data2)) + ggtitle(file2_name)
-)
+  plot_grid(
+    plot.per.sequence.quality.scores(per_sequence_quality_scores(json_data1)) + ggtitle(file1_name),
+    plot.per.sequence.quality.scores(per_sequence_quality_scores(json_data2)) + ggtitle(file2_name)
+  )
 
-plot_grid(
-  plot.per.base.sequence.content(per_base_sequence_content(json_data1)) + ggtitle(file1_name),
-  plot.per.base.sequence.content(per_base_sequence_content(json_data2)) + ggtitle(file2_name),
-  nrow = 2
-)
+  plot_grid(
+    plot.per.base.sequence.content(per_base_sequence_content(json_data1)) + ggtitle(file1_name),
+    plot.per.base.sequence.content(per_base_sequence_content(json_data2)) + ggtitle(file2_name),
+    nrow = 2
+  )
 
-plot_grid(
-  plot.per.base.N.count(per_base_N_count(json_data1)) + ggtitle(file1_name),
-  plot.per.base.N.count(per_base_N_count(json_data2)) + ggtitle(file2_name),
-  nrow=2
-)
+  plot_grid(
+    plot.per.base.N.count(per_base_N_count(json_data1)) + ggtitle(file1_name),
+    plot.per.base.N.count(per_base_N_count(json_data2)) + ggtitle(file2_name),
+    nrow=2
+  )
 
-plot_grid(
-  plot.seq.duplication.levels(sequence_duplication_levels(json_data1)) + ggtitle(file1_name),
-  plot.seq.duplication.levels(sequence_duplication_levels(json_data2)) + ggtitle(file2_name),
-  nrow = 2
-)
+  plot_grid(
+    plot.seq.duplication.levels(sequence_duplication_levels(json_data1)) + ggtitle(file1_name),
+    plot.seq.duplication.levels(sequence_duplication_levels(json_data2)) + ggtitle(file2_name),
+    nrow = 2
+  )
+
+
 
